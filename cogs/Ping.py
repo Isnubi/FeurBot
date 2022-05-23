66f973d50fba4c41915a64bb382d42ad30335200
Isnubi <isnubiyt@gmail.com>
2022-05-23 15:00:18 +0200

    Remove the deletion of the message in the ping command.

--- cogs/Ping.py
+++ cogs/Ping.py
@@ -10,8 +10,6 @@ class Ping(commands.Cog):
     async def ping(self, ctx):
         embed = discord.Embed(title="Pong!", description='Bot have {0}ms of delay'.format(round(self.bot.latency * 1000)), color=discord.Colour.blue())
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-
-        await ctx.message.delete()
         await ctx.send(embed=embed)
 
 
