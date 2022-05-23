77a952be8ca02fd4074677a64d70c1690a0af1a9
Isnubi <isnubiyt@gmail.com>
2022-05-23 18:47:16 +0200

    Add roll command to help menu. Modify roll command output : deletion of output and addition of embed output.

--- cogs/Roll.py
+++ cogs/Roll.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext import commands
 import random
 
@@ -11,31 +12,14 @@ class Roll(commands.Cog):
         try:
             rolls, limit = args.split('d')
         except ValueError:
-            await ctx.send('Format has to be in NdN!')
+            await ctx.send('Format has to be in [number]d[number]!')
             return
         try:
             rolls = int(rolls)
             limit = int(limit)
         except ValueError:
-            await ctx.send('Format has to be in NdN!')
+            await ctx.send('Format has to be in [number]d[number]!')
             return
-        if rolls > 100:
-            await ctx.send('You can\'t roll more than 100 dice at once!')
-            return
-        if limit > 100:
-            await ctx.send('You can\'t roll more than 100 sides on a dice!')
-            return
-        if rolls <= 0:
-            await ctx.send('You can\'t roll less than 1 dice!')
-            return
-        if limit <= 0:
-            await ctx.send('You can\'t roll less than 1 side on a dice!')
-            return
-        if rolls > 1:
-            msg = 'Rolling {} dice with {} sides each...'.format(rolls, limit)
-        else:
-            msg = 'Rolling {} die with {} sides...'.format(rolls, limit)
-        await ctx.send(msg)
         total = 0
         dice_rolls = []
         for i in range(rolls):
@@ -43,8 +27,13 @@ class Roll(commands.Cog):
             dice_rolls.append(dice_roll)
         for i in dice_rolls:
             total += i
-        await ctx.send('Total: {}'.format(total))
-        await ctx.send('Rolls: {}'.format(dice_rolls))
+
+        embed = discord.Embed(title='Dices roll', description='You just roll **{0}** with **{1}** sides each'.format(rolls, limit), color=discord.Colour.blue())
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        embed.add_field(name='Detailed rolls', value=dice_rolls, inline=False)
+        embed.add_field(name='Total', value=total, inline=False)
+
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
