2b21f315c995485be718f85606ed85b9ce3a608f
Louis GAMBART <louis.gambart77270@gmail.com>
2022-09-20 12:26:15 +0200

    Update mysql script Remove JSON files in order to use database instead

--- FeurBot.sql
+++ FeurBot.sql
@@ -8,7 +8,8 @@ use FeurBot;
 
 create table guilds
 (
-    guild_id        varchar(150) not null,
+    guild_id        varchar(150) not null
+        primary key,
     guild_name      varchar(500) not null,
     welcome_channel varchar(150) null,
     system_channel  varchar(150) null
@@ -24,4 +25,14 @@ create table users
     user_experience int        default 0 null,
     user_money      int                  null,
     `user_is-daily` tinyint(1) default 0 null
+);
+
+create table pnj
+(
+    guild_id   varchar(150) not null,
+    channel_id varchar(150) not null,
+    pnj_id     int auto_increment
+        primary key,
+    pnj_name   varchar(150) not null,
+    pnj_image  varchar(500) null
 );
